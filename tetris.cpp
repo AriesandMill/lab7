516287c0c045e79e4ed1f3ddb2575a6af5288c1a
Aries <tommysmart23@yahoo.com.tw>
2016-06-14 11:30:06 +0800

    lab8

--- tetris.cpp
+++ tetris.cpp
@@ -6,20 +6,20 @@
 using namespace std;
 int main()
 {
-	I_Block i, *ip = &i;
+	int i = 0,k; 
 	I_Block *ipi[50];
-	try { for(int k=0;k<50;++k)
-	ipi[k] = new I_Block[5000000];
+	try { 
+		for(k=0;k<50;++k){
+			ipi[k] = new I_Block[50000000];
+			cout<<"new I_Block["<<k<<"]"<<endl;
+		}
 	}catch(bad_alloc &iii){
-		cout <<"delete"<<endl;
-		delete[] ipi;
-	}
-	S_Block s,*sp = &s;
-	i.paint();
-	ip->paint();																	
-	s.paint();
-	sp->paint();
-	ip = &s;
-	ip->paint();																						
-	return 0;
+		while(i <= k){
+			cout <<"delete I_Block["<<i<<"]"<<endl;
+			delete[] ipi[i];
+			i++;
+		}
+	}    
+cout<<"When new 50000000*"<<k<<" I_Blocks, the memory is insufficient! \n"<<"Finally,this program delete to I_Block["<<i-1<<"]"<<endl;
+					    return 0;
 }
